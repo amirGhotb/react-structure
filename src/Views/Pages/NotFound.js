36962d154bb4cefa883cb79ee2c62accf8b53039
AmirHossein_Ghotbeddini <43760972+amirGhotb@users.noreply.github.com>
2021-10-03 09:38:51 +0330

    add flow

--- src/Views/Pages/NotFound.js
+++ src/Views/Pages/NotFound.js
@@ -1,3 +1,13 @@
-export default function NotFound(){
-    return <div>notfound</div>
+// @flow
+import * as React from "react";
+export default function NotFound(): React.Node{
+
+    function foo(x: ?string): string {
+        if (x) {
+            return x;
+        }
+        return "default string";
+    }
+
+    return <div>{foo('25')}</div>
 }
